4089079beb7906203894713fb549705882b8ca96
srenkel <sebastian.renkel@kaarsterheide.de>
2016-01-18 09:18:46 +0100

    FIXED game on mobile

--- js/game/engine.js
+++ js/game/engine.js
@@ -383,7 +383,7 @@ var TouchControls = function() {
     var yLoc = Game.height - unitWidth;
     this.drawSquare(ctx,gutterWidth,yLoc,"\u25C0", Game.keys['left']);
     this.drawSquare(ctx,unitWidth + gutterWidth,yLoc,"\u25B6", Game.keys['right']);
-    this.drawSquare(ctx,4*unitWidth,yLoc,"A",Game.keys['fire']);
+    this.drawSquare(ctx,4*unitWidth,yLoc,"A",Game.keys['enter']);
 
     ctx.restore();
   };
